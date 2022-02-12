6af1eb50bb3a4f277f32cc4af05cccc89184cbad
Y0up <alexis.dangelo.marquine@gmail.com>
2022-02-12 14:47:50 +0100

    add image upload tech

--- src/Controller/Security/SecurityController.php
+++ src/Controller/Security/SecurityController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller;
+namespace App\Controller\Security;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,7 +10,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/login", name="app_login")
+     * @Route("/admin/login", name="app_login")
      */
     public function login(AuthenticationUtils $authenticationUtils): Response
     {
